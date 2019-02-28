49570fc770ec7bcfbe70e3069cee1d343d9b3893
Morgan Pichat <Pichat.morgan@gmail.com>
2019-02-28 14:13:48 +0100

    Creation du projet

---
+++ src/main/java/com/bibliotheque/Projet7WebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bibliotheque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projet7WebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projet7WebServiceApplication.class, args);
+	}
+
+}
