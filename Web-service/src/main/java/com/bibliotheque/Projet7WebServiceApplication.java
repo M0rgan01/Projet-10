3e2e5118c0cf1727e53efec6c9ee3b47c3deed53
Morgan <pichat.morgan@gmail.com>
2019-08-05 18:44:27 +0200

    Correction ticket 2

--- Web-service/src/main/java/com/bibliotheque/Projet7WebServiceApplication.java
+++ Web-service/src/main/java/com/bibliotheque/Projet7WebServiceApplication.java
@@ -1,45 +1,5 @@
-<<<<<<< Updated upstream
+
 package com.bibliotheque;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
-
-import com.bibliotheque.dao.BookRepository;
-
-@SpringBootApplication
-@PropertySources({
-    @PropertySource("classpath:bibliotheque.properties"),
-    @PropertySource(value ="file:${catalina.home}/conf/Projet7-WebService/bibliotheque.properties", ignoreResourceNotFound = true),
-    @PropertySource(value ="file:${catalina.home}/conf/Projet7-WebService/application.properties", ignoreResourceNotFound = true),
-})
-public class Projet7WebServiceApplication extends SpringBootServletInitializer implements CommandLineRunner {
-
-	
-	@Autowired
-	private BookRepository bookRepository;
-
-	public static void main(String[] args) {
-		SpringApplication.run(Projet7WebServiceApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-
-	bookRepository.findAll().forEach(book ->{
-		System.out.println(book.getTitle());
-	});
-	}
-
-}
-=======
-package com.bibliotheque;
-
-import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -56,13 +16,6 @@ import com.bibliotheque.dao.MailRepository;
 import com.bibliotheque.dao.ReservationRepository;
 import com.bibliotheque.dao.RolesRepository;
 import com.bibliotheque.dao.UserRepository;
-import com.bibliotheque.entities.Book;
-import com.bibliotheque.entities.Kind;
-import com.bibliotheque.entities.Loan;
-import com.bibliotheque.entities.Mail;
-import com.bibliotheque.entities.Reservation;
-import com.bibliotheque.entities.Roles;
-import com.bibliotheque.entities.User;
 import com.bibliotheque.metier.LoanBusiness;
 import com.bibliotheque.metier.ReservationBusiness;
 import com.bibliotheque.metier.UserBusiness;
@@ -111,4 +64,4 @@ public class Projet7WebServiceApplication extends SpringBootServletInitializer i
 		//loanBusiness.createLoan(4l, 2l);
 	}
 }
->>>>>>> Stashed changes
+
